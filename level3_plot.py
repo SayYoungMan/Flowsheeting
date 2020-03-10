fe6b95c5c421c8d36896088367f0a31be780f90c
SayYoungMan <nickte89@gmail.com>
2020-03-10 13:30:44 +0000

    df

--- level3_plot.py
+++ level3_plot.py
@@ -29,4 +29,45 @@ def ep3_optimal():
     plt.xlim([0, 25])
     plt.show()
 
-heating_method()
+def ep3_no_heat():
+    df = pd.read_excel("Data/new graph seyoung 06032020 (1).xlsx", sheet_name="Sheet2", usecols='B:T').to_numpy()
+    plt.plot(df[:, 0], df[:, 18] / 1000000)
+    plt.ylabel("Economic Potential (Million USD/yr)")
+    plt.xlabel("Conversion (%)")
+    plt.xlim([0, 26])
+    plt.show()
+
+def ada_iso():
+    df = pd.read_excel("Data/Adiabatic vs isothermal.xlsx", sheet_name="Summary", usecols='D:K', skiprows=2)
+    iso = pd.read_excel("Data/Adiabatic vs isothermal.xlsx", sheet_name="Summary", usecols='M:O', skiprows=1).to_numpy()
+
+    cov = df.loc[0].to_numpy().reshape(-1)
+    sixsix = df.loc[4].to_numpy().reshape(-1)
+    sixeight = df.loc[5].to_numpy()
+    sevenzero = df.loc[6].to_numpy()
+    seventwo = df.loc[7].to_numpy()
+    sevenfour = df.loc[9].to_numpy()
+    sevensix = df.loc[10].to_numpy()
+    seveneight = df.loc[11].to_numpy()
+
+    plt.plot(cov, sixsix / 1000000, label="660K", color = 'blue')
+    plt.plot(cov, sixeight / 1000000, label="680K", color = 'aqua')
+    plt.plot(cov, sevenzero / 1000000, label="700K", color = 'darkgreen')
+    plt.plot(cov, seventwo / 1000000, label="720K", color = 'limegreen')
+    plt.plot(cov, sevenfour / 1000000, label="740K", color = 'yellow')
+    plt.plot(cov, sevensix / 1000000, label="760K", color = 'darkorange')
+    plt.plot(cov, seveneight / 1000000, label="780K", color = 'red')
+
+    plt.plot(iso[:, 2], iso[:, 0] / 1000000, linestyle='--', label="Isothermal", color = 'indigo')
+    plt.hlines(0,0,100,linestyles="--")
+    plt.legend(loc = 'lower left')
+    plt.xlim([0, 30])
+    plt.ylim([-25, 40])
+    plt.ylabel("Economic Potential (Million USD/yr)")
+    plt.xlabel("Conversion (%)")
+    plt.show()
+
+
+
+
+ada_iso()
